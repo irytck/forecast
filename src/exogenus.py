e4797f781773ce56cda81a2d58df917febf84cf1
irytck <rj.lince@gmail.com>
2024-11-05 16:24:29 +0100

    Exogenus variables

---
+++ src/exogenus.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov  5 16:09:16 2024
+
+@author: user
+"""
+
+# Import libraries
+
+# Load Data
+
+# Exogenus variables
+'''
+- Crude Oil Production, Total OPEC, Non-OPEC, World
+- Petroleum Consumption, Total OECD
+- Kilian Global economic index
+- EU 28 Countries: PPI
+- Petroleum inventory, Total OECD
+- The exchange rate of euro against US dollar
+- S&P 500 index
+- Dow Jones Index
+- NASDAQ index
+- COMEX: Gold: Future closing price
+- LME: Copper: Future closing price
+- WTI-Brent spot price spread
+- WTI crack spread: actual value
+- Brent crack spread: actual value'''
+
+
